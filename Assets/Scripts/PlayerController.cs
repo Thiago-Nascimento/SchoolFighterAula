5206fa8c4f19af8dc40464aef86189ad5ea20e90
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2024-11-21 17:23:51 -0300

    feat: camera iniciada

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,42 @@ public class PlayerController : MonoBehaviour
     public float playerSpeed = 1f;
 
     public Vector2 playerDirection;
+
+    private bool isWalking;
+
+    private Animator playerAnimator;
+
+    // Player olhando para a direita
+    private bool playerFacingRight = true;
     
     void Start()
     {
         // Obtem e inicializa as propriedades do RigidBody2D
         playerRigidBody = GetComponent<Rigidbody2D>();
+
+        // Obtem e inicializa as propriedades do animator
+        playerAnimator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
         PlayerMove();
+        UpdateAnimator();
+    }
+
+    // Fixed Update geralmente é utilizada para implementação de física no jogo
+    // Por ter uma execução padronizada em diferentes dispositivos
+    private void FixedUpdate()
+    {
+        // Verificar se o Player está em movimento
+        if (playerDirection.x != 0 || playerDirection.y != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
 
         playerRigidBody.MovePosition(playerRigidBody.position + playerSpeed * Time.fixedDeltaTime * playerDirection);
     }
@@ -24,5 +50,35 @@ public class PlayerController : MonoBehaviour
     {
         // Pega a entrada do jogador, e cria um Vector2 para usar no playerDirection
         playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // Se o player vai para a ESQUERDA e está olhando para a DIREITA
+        if (playerDirection.x < 0 && playerFacingRight)
+        {
+            Flip();
+        }
+
+        // Se o player vai para a DIREITA e está olhando para ESQUERDA
+        else if (playerDirection.x > 0 && !playerFacingRight)
+        {
+            Flip();
+        }
+    }
+
+    void UpdateAnimator()
+    {
+        // Definir o valor do parâmetro do animator, igual à propriedade isWalking
+        playerAnimator.SetBool("isWalking", isWalking);
+    }
+
+    void Flip()
+    {
+        // Vai girar o sprite do player em 180º no eixo Y
+
+        // Inverter o valor da variável playerfacingRight
+        playerFacingRight = !playerFacingRight;
+
+        // Girar o sprite do player em 180º no eixo Y
+        // X, Y, Z
+        transform.Rotate(0, 180, 0);
     }
 }
